e915d6184f8129758e0f4a65da30ed01886b21c1
Ignatiusdork <littignatius@gmail.com>
2024-04-05 15:30:02 +0100

    migirations for the carts_items table

---
+++ database/migrations/2024_04_05_142635_cart_items.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->ID();
+            $table->unsignedBigInteger('cart_id');
+            $table->unsignedInteger('product_id');
+            $table->integer('quanity');
+            $table->decimal('price', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
