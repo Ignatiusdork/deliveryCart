7dd1d60baf95d9388c7b8e2f271b3d4f7220a47a
Ignatiusdork <littignatius@gmail.com>
2024-04-12 16:11:29 +0100

    db migrations

--- database/migrations/2024_04_12_150235_order_items.php
+++ database/migrations/2024_04_12_150235_order_items.php
@@ -11,7 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('quanity');
+            $table->decimal('price', 8, 2);
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
     }
 
     /**
