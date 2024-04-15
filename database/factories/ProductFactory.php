8969a588c4b02f53b19a495d4c887eb7673a87a9
Ignatiusdork <littignatius@gmail.com>
2024-04-15 17:12:43 +0100

    db population with data

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -20,7 +20,7 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' =>$this->faker->word(),
+            'name' => $this->faker->word(),
             'description' => $this->faker->sentence,
             'price' => $this->faker->randomFloat(2, 1, 100),
             'stock' => $this->faker->randomFloat(1, 100),
