2d3273417e89d6e924e4b396e8bec3bc9e142a94
Ignatiusdork <littignatius@gmail.com>
2024-04-20 15:38:51 +0100

    views-implementation

---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+
+
+    @section('content')
+        <h1>Products</h1>
+        @foreach ($products as $product)
+            <div>
+                <h2>{{ $product->name }}</h2>
+                <p>{{ $product->description }}</p>
+                <p>Price: ${{ $product->price }}</p>
+            </div>
+        @endforeach
+    @endsection
+
