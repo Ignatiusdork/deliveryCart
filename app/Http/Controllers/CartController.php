353b06e3c6cea62899b093ec0086b60bd056d4d2
Ignatiusdork <littignatius@gmail.com>
2024-06-09 12:44:18 +0100

    cart-controller-updates

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+    public function add(Request $request, $id)
+    {
+        //retrieve the product from the database
+        $product = \App\Models\Product::find($id);
+    }
 }
