a0e9e237c621044a2b53ff872ab5670bc7086467
Ignatiusdork <littignatius@gmail.com>
2024-06-14 15:17:28 +0100

    cart-controller-update

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -5,16 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use App\Models\Cart;
+use App\Models\Product;
 
 class CartController extends Controller
 {
     public function add(Request $request, $id)
     {
         //retrieve the product from the database
-        $product = \App\Models\Product::find($id);
+        $product = Product::find($id);
 
-        //store the product in the session
-        $oldCart = Session::get('cart');
+        //get old cart data from the session or initialize a new array
+        $oldCart = Session::get('cart', []);
 
         $cart = new Cart(
             $oldCart? : [],
