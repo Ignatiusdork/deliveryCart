16f313f99a801a815777d5ea00cce161c797384c
Ignatiusdork <littignatius@gmail.com>
2024-06-25 15:51:32 +0100

    cart-controller-update

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -9,4 +9,21 @@ class CartService
 {
     protected $items = [];
     protected $sessionKey = 'cart';
+
+    public function __construct()
+    {
+        $this->loadFromSession();
+    }
+
+    public function add(Product $product, int $quantity = 1)
+    {
+        $id = $product->id;
+        if (isset($this->items[$id])) {
+            $this->items[$id]['quantity'] += $quantity;
+        } else {
+            $this->items[$id] = ['item' => $product, 'quantity' => $quantity];
+        }
+
+        $this->saveSession();
+    }
 }
