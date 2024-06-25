895d48689105edd46088e40f7ac37ae201d13c4d
Ignatiusdork <littignatius@gmail.com>
2024-06-25 15:42:20 +0100

    cart-controller-update

--- app/Services/CartService.php
+++ app/Services/CartService.php
@@ -4,3 +4,9 @@ namespace App\Services;
 
 use Illuminate\Support\Facades\Session;
 use App\Models\Product;
+
+class CartService
+{
+    protected $items = [];
+    protected $sessionKey = 'cart';
+}
