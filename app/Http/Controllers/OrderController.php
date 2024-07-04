5aeab3c7f89d3adefadfb39b7a4235f46983f715
Ignatiusdork <littignatius@gmail.com>
2024-07-04 16:04:24 +0100

    orders-update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\OrderService;
+use App\Services\CartService;
+use Illuminate\Support\Facades\Auth;
+
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    protected $orderService;
+    protected $cartService;
+
+    public function __construct(OrderService $orderService, CartService $cartService)
+    {
+        $this->orderService = $orderService;
+        $this->cartService = $cartService;
+    }
+
+    public function placeOrder()
+    {
+
+    }
 }
--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -7,4 +7,41 @@ use App\Models\OrderItem;
 use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
 
+class OrderService
+ {
+    public function placeOrder($items, $totalPrice)
+    {
+        $order = Order::create(([
+            'user_id' => Auth::id(),
+            'status' => 'Pending',
+            'total' => $totalPrice,
+        ]));
 
+        foreach ($items as $item) {
+            OrderItem::create([
+                'order_id' => $order->id,
+                'product_id' => $item['id'],
+                'quantity' => $item['quantity'],
+                'price' => $items['item']->price,
+        ]);
+
+        // update product stock
+        $product = Product::find($item['item']->id);
+        $product->stock -= $item['quantity'];
+        $product->save();
+
+        }
+
+        return $order;
+    }
+
+    public function getOrder($id)
+    {
+        return Order::with('orderItems.product')->findOrFail($id);
+    }
+    
+    public function getUserOrders()
+    {
+        return Order::where('user_id', Auth::id())->get();
+    }
+ }
