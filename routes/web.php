da29636d2e674ecac7ab4860f5fc2065f5977c1a
Ignatiusdork <littignatius@gmail.com>
2024-07-15 14:49:53 +0100

    admin_auth

--- routes/web.php
+++ routes/web.php
@@ -28,11 +28,18 @@ Route::get('/cart', [CartController::class, 'viewCart'])->name('cart.view');
 Route::post('/cart/add/{id}', [CartController::class, 'add'])->name('cart.add');
 Route::delete('/cart/remove/{id}', [CartController::class, 'remove'])->name('cart.remove');
 
+    // Routes accessible to all authenticated users
 Route::middleware('auth')->group(function () {
     Route::post('/order/place', [OrderController::class, 'placeOrder'])->name('order.place');
     Route::get('/order/{id}', [OrderController::class, 'showOrder'])->name('orders.show');
     Route::get('/orders', [OrderController::class, 'listOrders'])->name('orders.index');
-    Route::post('/orders/{order}/status', [OrderController::class, 'update'])->name('orders.index');
+});
+
+    // Routes accessible only to admin users
+Route::middleware(['auth', 'is_admin'])->group(function () {
+    Route::post('/orders/{order}/status', [OrderController::class, 'update'])->name('orders.updateStatus');
+    Route::patch('/orders/{orderId}/shipped', [OrderController::class, 'markAsShipped'])->name('orders.markAsShipped');
+    Route::patch('/orders/{orderId}/delivered', [OrderController::class, 'markAsDelivered'])->name('orders.markAsDelivered');
 });
 
 Route::get('/dashboard', function () {
