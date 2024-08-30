387075e6bf3abdb337ceda072e38169a9434ce3a
Ignatiusdork <littignatius@gmail.com>
2024-08-30 17:24:07 +0100

    user-dashboard-update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,23 +1,23 @@
 <x-app-layout>
 
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight flex justify-center items-center min-h-screen">
-            <a href="{{ route('products.index') }}" class="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded text-xl">
+        <div class="flex justify-center items-start min-h-[3rem]">
+            <a href="{{ route('products.index') }}" class="bg-blue-500 hover:bg-blue-700 text-black font-bold py-3 px-6 rounded text-lg mt-4">
                 View Products
             </a>
-        </h2>
+        </div>
     </x-slot>
 
-    <x-slot name="body">
+    @section('content')
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        {{ __("Welcome to Radiance Shop Cart") }}
+                    <div class="p-6 text-gray-900 dark:text-gray-800">
+                        {{ __("Welcome to Ofon's Shop Cart") }}
                     </div>
                 </div>
             </div>
         </div>
-    </x-slot>
-
+    @endsection
+    
 </x-app-layout>
