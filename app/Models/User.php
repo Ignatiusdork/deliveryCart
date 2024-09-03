926578cbb875806a8eb69397997719762f1cf567
Ignatiusdork <littignatius@gmail.com>
2024-09-03 19:59:38 +0100

    WHMCS-integration

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,9 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Jetstream\HasTeams;
+
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
@@ -62,6 +60,6 @@ class User extends Authenticatable
     }
 
     public function ticket() {
-        
+        return $this->hasMany(Ticket::class);
     }
 }
