b403285e54283a18ce7a19d11680a60a0594fa41
Ignatiusdork <littignatius@gmail.com>
2024-09-08 08:38:54 +0100

    ticket-service

--- app/Services/TicketService.php
+++ app/Services/TicketService.php
@@ -1,8 +1,25 @@
 <?php
 
-
 namespace App\Services;
 
 use App\Models\Ticket;
 use App\Models\TicketReply;
 use Illuminate\Support\Facades\Validator;
+
+class TicketService {
+    public function createTicket(array $data) {
+        Validator::make($data, [
+            'subject' => 'required|string|max:255',
+            'message' => 'required|string',
+        ])->validate();
+
+        $ticket = Ticket::create([
+            'user_id' => auth()->id(),
+            'subject' => $data['subject'],
+            'message' => $data['message'],
+            'status' => 'open'
+        ]);
+
+        return $ticket;
+    }
+}
