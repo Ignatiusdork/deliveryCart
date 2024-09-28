4166c69641067a19c2b844d015d3e9e0f4dcd1e5
Ignatiusdork <littignatius@gmail.com>
2024-09-28 22:42:28 +0100

    order_number_db_update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Product;
+use App\Models\Order;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -22,6 +23,15 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         // Product::factory()->count(30)->create();
+
+        Order::all()->each(function ($order) {
+            $parts = explode('-', $order->created_at->format('Y-m'));
+            $order->update([
+                'year' => intval($parts[0]),
+                'month' => intval($parts[1]),
+                'order_number' => sprintf('%04d%s001', $parts[0], $parts[1]),
+            ]);
+        });
 
         $this->call([
             PopulateTempUserIdSeeder::class,
